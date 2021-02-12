8a6014c1d1ef32d4c7db47da6d89f50b5ab80d9c
shourov-sorkar <18103142@iubat.edu>
2021-02-13 03:28:20 +0600

    :heavy_check_mark: add sidebar section and add compose button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Header from "./Component/Layout/Header";
+import Sidebar from "./Component/Sidebar";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Sidebar />
     </div>
   );
 }
